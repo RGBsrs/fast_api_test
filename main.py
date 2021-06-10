a355e8cfd0a78573c2f037cab97d01229f849f4a
Dmytro <grjanko.d.a@gmail.com>
2021-06-10 11:35:22 +0300

    reduced code repeatability

--- main.py
+++ main.py
@@ -15,18 +15,24 @@ drinks_str = [drink['strCategory'] for drink in drinks]
 
 
 @app.get("/", response_class=HTMLResponse)
+@app.post("/", response_class=HTMLResponse)
 async def root(request: Request):
-    result_group = {}
-    for drink in drinks_str:
-        result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink }')
-        result_group[drink] = result.json()['drinks']  
-    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group, 'drinks_list': drinks_str, 'filter': drinks_str})
-
-@app.post("/")
-async def filter(request: Request):
-    filtered_drinks = await request.form()
-    result_group = {}
+    results = {}
+    if request.method == 'POST':
+        filtered_drinks = await request.form()
+    else:
+        filtered_drinks = drinks_str
+    
     for drink in filtered_drinks:
         result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink }')
-        result_group[drink] = result.json()['drinks']
-    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group, 'drinks_list': drinks_str, 'filter': filtered_drinks})
+        results[drink] = result.json()['drinks']  
+    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : results, 'drinks_list': drinks_str, 'filter': filtered_drinks})
+
+# @app.post("/")
+# async def filter(request: Request):
+#     filtered_drinks = await request.form()
+#     result_group = {}
+#     for drink in filtered_drinks:
+#         result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink }')
+#         result_group[drink] = result.json()['drinks']
+#     return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group, 'drinks_list': drinks_str, 'filter': filtered_drinks})
