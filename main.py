e2b4bc364311f6f1115a59fa6177e878354b797c
Dmytro <grjanko.d.a@gmail.com>
2021-06-10 11:15:40 +0300

    add filtering

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import httpx
@@ -8,14 +9,24 @@ app = FastAPI()
 client = httpx.AsyncClient()
 
 templates = Jinja2Templates(directory="templates")
+drinks_list = httpx.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+drinks = drinks_list.json()['drinks']
+drinks_str = [drink['strCategory'] for drink in drinks]
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    drinks_list = httpx.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
-    drinks = drinks_list.json()['drinks']
     result_group = {}
-    for drink in drinks:
-        result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink["strCategory"] }')
-        result_group[drink["strCategory"]] = result.json()['drinks']  
-    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group})
+    for drink in drinks_str:
+        result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink }')
+        result_group[drink] = result.json()['drinks']  
+    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group, 'drinks_list': drinks_str, 'filter': drinks_str})
+
+@app.post("/")
+async def filter(request: Request):
+    filtered_drinks = await request.form()
+    result_group = {}
+    for drink in filtered_drinks:
+        result = await client.get(f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={ drink }')
+        result_group[drink] = result.json()['drinks']
+    return templates.TemplateResponse("gallery.html", {"request": request, 'results' : result_group, 'drinks_list': drinks_str, 'filter': filtered_drinks})
